039e2a403758cfe2166978c3e90835a1f74548d7
andrios5 <74190232+andrios5@users.noreply.github.com>
2025-04-13 15:50:44 -0300

    Update v1.02

--- dockerfile
+++ dockerfile
@@ -1,2 +1,4 @@
-FROM nginx
-COPY nginx.conf /etc/nginx/nginx.conf
+FROM httpd:2.4
+WORKDIR /usr/local/apache2/htdocs/
+COPY . /usr/local/apache2/htdocs/
+EXPOSE 80
--- index.php
+++ index.php
@@ -20,7 +20,7 @@ header('Content-Type: text/html; charset=iso-8859-1');
 
 echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 
-$servername = "192.168.1.124";
+$servername = "192.168.1.150";
 $username = "root";
 $password = "3570";
 $database = "meubanco";
